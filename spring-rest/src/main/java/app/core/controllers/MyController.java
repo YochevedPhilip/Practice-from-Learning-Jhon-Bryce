b9268c015170bd295b8b373e5254671757bfa492
פיליפ <@192.168.1.2>
2023-03-30 19:06:05 +0300

    spring rest

--- spring-rest/src/main/java/app/core/controllers/MyController.java
+++
@@ -1,6 +0,0 @@
-package app.core.controllers;
-
-public class MyController {
-	
-
-}
---
+++ spring_rest/src/main/java/app/core/controllers/MyController.java
@@ -0,0 +1,19 @@
+package app.core.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController { 
+	
+	
+
+		
+		@RequestMapping(method = RequestMethod.GET, path = "/hello")
+		public String helloWorld() {
+			return "Hello world";
+		}
+
+
+}
